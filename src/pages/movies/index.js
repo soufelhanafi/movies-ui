0729f3d68c98a2fac6247a27420896fe27f19fc0
Soufian Elhanafi <soufelhanafi@gmail.com>
2019-12-03 17:17:22 +0100

    configure react router

---
+++ src/pages/movies/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Movies extends React.Component {
+  render() {
+    return <h1>Movies</h1>;
+  }
+}
+
+export default Movies;
---
+++ src/router.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Route, Redirect, BrowserRouter } from "react-router-dom";
+
+import Dashboard from "./pages/dashboard";
+import Movies from "./pages/movies";
+
+const routes = [
+  {
+    path: "/dashboard",
+    component: Dashboard
+  },
+  {
+    path: "/movies",
+    component: Movies
+  }
+];
+class Router extends React.Component {
+  render() {
+    debugger;
+    return (
+      <BrowserRouter>
+        {routes.map((route, index) => {
+          return (
+            <Route path={route.path} component={route.component} key={index} />
+          );
+        })}
+        <Route render={() => <Redirect to={"/dashboard"} />} />
+      </BrowserRouter>
+    );
+  }
+}
+
+export default Router;
